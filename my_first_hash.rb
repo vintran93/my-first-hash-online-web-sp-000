ea3f79cb193261cfabc71dcf8ebafa14d1f82e49
Vincent Tran <vincentuong@yahoo.com>
2021-05-23 04:40:33 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,6 +35,7 @@ def adding
   # add 2 muskets to the shipping_manifest hash below
 
 shipping_manifest["muskets"] => 2 
+shipping_manifest["gun powder"] => 4 
 
   # add 4 gun powder to the shipping_manifest hash below
 
